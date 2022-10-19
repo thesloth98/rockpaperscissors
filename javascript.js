154489b4d30146094f4d137c397cc3b4e181050a
thesloth98 <aleksandrabalkanska@gmail.com>
2022-10-20 00:15:18 +0900

    Need to correct loop add scoring

--- javascript.js
+++ javascript.js
@@ -27,41 +27,67 @@ const playerSelection =  prompt("Pick among 'Rock, Paper, Scissors' to start the
 
 const computerSelection = computerChoice();
 
-const playRound = (playerSelection, computerSelection) => {
+function playRound(playerSelection, computerSelection) {
                 if (playerSelection === computerSelection) {
-                    return `It\'s a tie! You both picked ${playerSelection} `;
+                    console.log(`It\'s a tie! You both picked ${playerSelection} `);
                 } else if (playerSelection === 'rock' && computerSelection ==='scissors') {
                     playerScore++;
-                    return `You Win! ${playerSelection} beats ${computerSelection}!`;
+                    console.log(`You Win! ${playerSelection} beats ${computerSelection}!`);
                 } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
                     playerScore++;
-                    return `You Win! ${playerSelection} beats ${computerSelection}!`;
+                    console.log(`You Win! ${playerSelection} beats ${computerSelection}!`);
                 } else if (playerSelection === 'paper' && computerSelection === 'rock') {
                     playerScore++;
-                    return `You Win! ${playerSelection} beats ${computerSelection}!`;
+                    console.log(`You Win! ${playerSelection} beats ${computerSelection}!`);
                 } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
                     computerScore++;
-                    return`You Lose! ${computerSelection} beats ${playerSelection}`;
+                    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
                 } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
                     computerScore++;
-                    return`You Lose! ${computerSelection} beats ${playerSelection}`;
+                    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
                 } else if (playerSelection === 'rock' && computerSelection === 'paper') {
                     computerScore++;
-                    return`You Lose! ${computerSelection} beats ${playerSelection}`;
+                    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
                 } else {
                     return `Something went wrong.`
                 }
 }
 
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection))
+// console.log(computerSelection);
+// console.log(playRound(playerSelection, computerSelection))
 
 //Looping the game
+function game() {
+    //Loop
+    for (let i = 0; 1 < 5; i++) {
+        let round = `Round: ${i+1}`;
+        console.log(round);
+        
+        if (i == 5) {
+            if (playerScore > computerScore) {
+            return 'Player is the winner.';
+          } else if (playerScore < computerScore) {
+            return 'Computer is the winner.';
+          } else {
+            return 'There is no game winner. We have a tie.';
+          }
+        } else if (i < 5 ) {
+            const playerSelection =  prompt("Pick among 'Rock, Paper, Scissors' to start the game'").toLowerCase();
+            const computerSelection = computerChoice();
+            console.log(playRound(playerSelection,computerSelection));
+        }
+    }
 
-// const game = () => {
+    console.log(`Player Score: ${playerScore} <-----> Computer Score: ${computerScore}`)
+
+}
+
+
+game();
+
+// function game() {
+// function playRound(playerSelection, computerSelection) {
 //  for (let i=0; i<5; i++) {
-//     playerPlay();
-//     computerChoice();
 //     if (i == 5) {
 //      if (playerScore > computerScore) {
 //         console. log ("Congrats! You beat the Computer!");
@@ -75,10 +101,29 @@ console.log(playRound(playerSelection, computerSelection))
 //     } else {
 //         return playRound();
 //     }
+//     }
 // }
 // }
 
 // const reset = () => {
 //     playerScore =0;
 //     computerScore=0;
+// }
+
+// function game() {
+//     for (let i=0; i < 5; i++) {
+//         const playerSelection = prompt("Pick among 'Rock, Paper, Scissors' to start the game'").toLowerCase();
+//         const computerSelection = computerChoice();
+//         if (i == 5) {
+//                  if (playerScore > computerScore) {
+//                     console. log ("Congrats! You beat the Computer!");
+//                  } else if (playerScore == computerScore) {
+//                    console.log ("It's a tie.");
+//                  } else {
+//                     console.log ("You lose. The Computer has beaten you.");
+//                  }
+//                  console.log(playerScore);
+//                  console.log(computerScore);
+//                 }
+//     }
 // }
